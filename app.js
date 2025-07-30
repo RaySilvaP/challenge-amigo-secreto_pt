161748c75bea0f147f198a20a53f1a7f3f0040a5
Ray Silva <silvaray355@gmail.com>
2025-07-30 15:54:27 -0300

    Adicionando funcionalidade de sortear um amigo

--- app.js
+++ app.js
@@ -1,1 +1,37 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const amigo = document.getElementById('amigo');
+const amigoSorteado = document.getElementById('resultado');
+const listaAmigos = document.getElementById('listaAmigos');
+let amigos = [];
+
+function adicionarAmigo() {
+    if(amigo.value === '') {
+        alert('Por favor, insira um nome.')
+    }
+    else {
+        amigos.push(amigo.value);
+        amigo.value = '';
+        atualizarLista();
+    }
+}
+
+function atualizarLista() {
+    listaAmigos.innerHTML = '';
+    amigos.forEach(a => {
+        const amigoItem = document.createElement('li');
+        amigoItem.innerHTML = a;
+        listaAmigos.appendChild(amigoItem);
+    })
+}
+
+function sortearAmigo() {
+    if(amigos.length === 0){
+        alert('Não há amigos para serem sorteados.');
+    }
+    else {
+        const indexSorteado = Math.floor(Math.random() * amigos.length);
+        const nome = amigos[indexSorteado];
+        amigoSorteado.innerHTML = `Seu amigo secreto é ${nome}!`;
+        listaAmigos.innerHTML = '';
+        amigos = [];
+    }
+}
